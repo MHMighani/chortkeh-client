7cb16f119e1676a71b4b648a44f98f7c4f99797b
MHMighani <mh.mgh2012@gmail.com>
2020-03-02 09:09:19 +0330

    Updates

---
+++ precache-manifest.b46f7cf069a96fac5e351f5a0ba70fa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfdb6c3dfe2a65676e32f4e782115a3",
+    "url": "/chortkeh/index.html"
+  },
+  {
+    "revision": "d70e689d4e9b019dbafa",
+    "url": "/chortkeh/static/js/2.f6147c10.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/chortkeh/static/js/2.f6147c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746f5089a76a78457ef6",
+    "url": "/chortkeh/static/js/main.2b3bc596.chunk.js"
+  },
+  {
+    "revision": "0fee0f0a1f71584c5ae5",
+    "url": "/chortkeh/static/js/runtime-main.ff247a2f.js"
+  }
+]);
