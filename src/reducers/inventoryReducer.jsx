65fe7818488ae6eb41639d83ee14cc9ae50aac47
MHMighani <mh.mgh2012@gmail.com>
2020-03-03 20:04:50 +0330

    duplicate inventory solved

--- src/reducers/inventoryReducer.jsx
+++ src/reducers/inventoryReducer.jsx
@@ -1,8 +1,16 @@
 import { ADD_TO_INVENTORY, DELETE_FROM_INVENTORY } from '../constants/actionTypes';
+import _ from 'lodash'
 
 export default (state = [], action) => {
 	switch (action.type) {
 		case ADD_TO_INVENTORY:
+            const duplicatedInventory = state.find(inventory=>inventory.subSource===action.payload.subSource)
+            if(duplicatedInventory){
+                const newState = state.map(inventory=>{
+                    return {...inventory,"amount":action.payload.amount}
+                })
+                return newState
+            }
 			return [...state, action.payload];
 		case DELETE_FROM_INVENTORY:
 			const newState = state.filter(inventory => inventory.subSource !== action.payload);
