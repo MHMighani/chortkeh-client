726ddcee2437e617fdcce6a752e812dfe8de5b61
MHMighani <mh.mgh2012@gmail.com>
2020-04-08 15:09:02 +0430

    fix: input fields restart after submiting

--- client/src/components/AddInventory.jsx
+++ client/src/components/AddInventory.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {reset} from 'redux-form'
 import AddInventoryForm from './forms/AddInventoryForm';
 import { addToInventory } from '../actions';
 import toast from 'toasted-notes'
 import 'toasted-notes/src/styles.css'
 
 class AddInventory extends Component {
-	handleSubmit = formValues => {
+	handleSubmit = (formValues,dispatch) => {
 		this.props.addToInventory(formValues);
+		
+		dispatch(reset('addInventoryForm'))
+
 		toast.notify('به دارایی اضافه شد',{
 			duration:2000
 		})
