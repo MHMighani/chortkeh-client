85c926f4f6114fd35d723eedfcbddd5ca2069960
MHMighani <mh.mgh2012@gmail.com>
2020-04-28 22:56:57 +0430

    feat: modal close by ESC click

--- client/src/components/modals/useModal.jsx
+++ client/src/components/modals/useModal.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const useModal = () => {
-  const [isShowing, setIsShowing] = useState(false);
+	const [isShowing, setIsShowing] = useState(false);
 
-  function toggle() {
-    setIsShowing(!isShowing);
-  }
+	const onEscKeyDown = (e) => {
+		if (!isShowing || e.key !== 'Escape') return;
+		setIsShowing(false);
+	};
 
-  return {
-    isShowing,
-    toggle,
-  }
+	useEffect(() => {
+    window.addEventListener('keydown', onEscKeyDown, false);
+    return () => {
+      window.removeEventListener('keydown',onEscKeyDown,false)
+    }
+	});
+
+	function toggle() {
+		setIsShowing(!isShowing);
+	}
+
+	return {
+		isShowing,
+		toggle,
+	};
 };
 
 export default useModal;
