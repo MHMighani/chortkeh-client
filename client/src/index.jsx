d5e9d018e74e03fc06869076e58e010cb6537d57
MHMighani <mh.mgh2012@gmail.com>
2020-05-10 17:28:42 +0430

    feat: data is saved in localstorage

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore,applyMiddleware,compose} from 'redux'
 import reduxThunk from 'redux-thunk'
+import {loadState,saveState} from './localStorage'
 
 import App from './components/App'
 import reducers from './reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducers,composeEnhancers(applyMiddleware(reduxThunk)))
+const persistedState = loadState();
+const store = createStore(reducers,persistedState,composeEnhancers(applyMiddleware(reduxThunk)))
+
+store.subscribe(() => {
+    saveState(store.getState())
+})
 
 ReactDOM.render(
     <Provider store={store}>
